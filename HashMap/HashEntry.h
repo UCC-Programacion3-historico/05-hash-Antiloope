6981d1e5570a6c870f6da8dce0964293b5617d08
Antiloope <rodrigopizarro1234@gmail.com>
2017-10-15 20:36:31 -0300

    Se completo la clase Hash

--- HashMap/HashEntry.h
+++ HashMap/HashEntry.h
@@ -4,9 +4,26 @@
 template<class T, class K>
 class HashEntry {
 private:
-
+    K key;
+    T dato;
 public:
+    HashEntry(K key, T dato) : key(key),dato(dato) {}
 
+    void setKey(K key) {
+        HashEntry::key = key;
+    }
+
+    void setDato(T dato) {
+        HashEntry::dato = dato;
+    }
+
+    K getKey() {
+        return key;
+    }
+
+    T getDato() {
+        return dato;
+    }
 };
 
 #endif //HASHENTRY_H
--- HashMap/HashMap.h
+++ HashMap/HashMap.h
@@ -1,9 +1,15 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
+#include "HashEntry.h"
+
 template<class K, class T>
 class HashMap {
 private:
+    HashEntry<K,T> **tabla;
+
+    int tam;
+
     unsigned int hashFunc(K clave);
 
     unsigned int (*hashFuncP)(K clave);
@@ -28,44 +34,74 @@ public:
 
 template<class K, class T>
 HashMap<K, T>::HashMap(unsigned int k) {
-
+    tam = k;
+    hashFuncP = hashFunc;
+    tabla = new HashEntry<K,T> *[tam];
+    for (int i = 0; i < tam; i++) {
+        tabla[i] = NULL;
+    }
 }
 
 template<class K, class T>
 HashMap<K, T>::~HashMap() {
-
+    for (size_t i = 0; i < tam; i++) {
+        if (tabla[i] != NULL) {
+            delete tabla[i];
+        }
+    }
 }
 
 template<class K, class T>
 T HashMap<K, T>::get(K clave) {
-    T temp;
-    return temp;
+    int pos = hashFuncP(clave) % tam;
+    if (tabla[pos] == NULL) {
+        throw 1;
+    }
+    return tabla[pos]->dato;
 }
 
 template<class K, class T>
 void HashMap<K, T>::put(K clave, T valor) {
-
+    int pos = hashFuncP(clave) % tam;
+    if (tabla[pos] != NULL) {
+        throw 2;
+    }
+    tabla[pos] = new HashEntry<K,T> (clave, valor);
 }
 
 template<class K, class T>
 void HashMap<K, T>::remove(K clave) {
-
+    int pos = hashFuncP(clave) % tam;
+    if (tabla[pos] == NULL) {
+        throw 3;
+    }
+    delete tabla[pos];
+    tabla[pos] = NULL;
 }
 
 template<class K, class T>
 bool HashMap<K, T>::esVacio() {
-    return false;
+    for (int i = 0; i < tam; i++) {
+        if (tabla[i] != NULL) {
+            return false;
+        }
+    }
+    return true;
 }
 
 template<class K, class T>
 unsigned int HashMap<K, T>::hashFunc(K clave) {
-    return 99999;
+    return clave;
 }
 
 template<class K, class T>
 HashMap<K, T>::HashMap(unsigned int k, unsigned int (*fp)(K)) {
-
+    tam = k;
+    hashFuncP = fp;
+    tabla = new HashEntry<K,T> *[tam];
+    for (int i = 0; i < tam; i++) {
+        tabla[i] = NULL;
+    }
 }
-
 
 #endif //HASHMAP_H
